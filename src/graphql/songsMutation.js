986ef46ee82d1a0ea1be2d33f55593e669932327
charkhaniakash <96294677+charkhaniakash@users.noreply.github.com>
2022-12-23 23:52:52 +0530

    Add files via upload

---
+++ src/graphql/songsMutation.js
@@ -0,0 +1,39 @@
+import { gql } from '@apollo/client';
+
+export const CREATE_NewSong = gql`mutation(
+    $name:String!,
+    $author_name:String,
+    $img:String!,
+    $lang:String,
+    $timesPlayed: Int, 
+    $musicName: String!,
+    $date_of_release:String!,
+    $date_of_birth:String!,
+    $other_songs:String!,
+    $artist_name:String
+){
+  createSong(
+    name:$name,
+    author_name:$author_name ,
+    img:$img
+    lang: $lang
+    timesPlayed:$timesPlayed 
+    musicName: $musicName
+    date_of_release: $date_of_release
+    date_of_birth: $date_of_birth
+    other_songs:  $other_songs
+    artist_name: $artist_name
+  ){
+        id
+        name
+        author_name
+        img
+        lang
+        timesPlayed
+        musicName
+        date_of_release
+        date_of_birth
+        other_songs
+  }
+}
+`;
