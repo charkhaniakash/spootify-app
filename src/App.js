079aac4d9e1694a933dcda10df51f12a51094da0
charkhaniakash <96294677+charkhaniakash@users.noreply.github.com>
2022-12-23 23:58:01 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-
+import {Route , Routes} from "react-router-dom"
 import './App.css';
+import AddSongs from "./components/AddSongs";
 import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
-    <Home/>
+    <Routes>
+      <Route path="/" element={<Home/>} ></Route>
+      <Route path="/add-Songs">{<AddSongs/>}</Route>
+    </Routes>
     </div>
   );
 }
